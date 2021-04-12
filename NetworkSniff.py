c157b16556c92101501d5c8668bd90bd7345ee66
LLighty <liam.hhui@gmail.com>
2021-04-12 20:59:09 +1000

    Basic python script which prints packets on loop back interface on port 5555 to console. Will change this later to take inputs from commandline to specify an output address and what details are wanted. i.e. just payload.

---
+++ NetworkSniff.py
@@ -0,0 +1,36 @@
+from scapy.all import *
+import platform
+
+error_code_unknown_platform = 1
+
+
+def main():
+    print("Hello World!")
+    sniff_loopback_port_packets(5555)
+
+
+def sniff_loopback_port_packets(port):
+    sniffer_filter = "port " + str(port)
+    interface = detect_loopback_interface()
+    if interface == error_code_unknown_platform:
+        print("Error finding OS aborting sniffing on loop back address")
+    print("Sniffing local packets")
+    sniff(filter=sniffer_filter, iface=interface, prn=analyse_packet)
+
+
+def analyse_packet(pkt):
+    print("Packet found")
+    pkt.show()
+
+
+def detect_loopback_interface():
+    print(str(platform.system()))
+    if platform.system() == "Linux":
+        return "lo"
+    if platform.system() == "Windows":
+        return "Software Loopback Interface 1"
+    return error_code_unknown_platform
+
+
+if __name__ == "__main__":
+    main()
